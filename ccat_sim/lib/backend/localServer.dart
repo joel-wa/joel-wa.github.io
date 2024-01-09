3fbe5b753ffa7c6c6bfbd0dfec46bf2b94844b38
joel-wa <UNILIFEGHANA@GMAIL.COM>
2024-01-09 12:45:24 +0530

    -AWS amplify

--- ccat_sim/lib/backend/localServer.dart
+++ ccat_sim/lib/backend/localServer.dart
@@ -60,133 +60,133 @@ class LocalServer {
     ),
     // //////////////////////
     // ///
-    // Question(
-    //   'Amy plans to buy 5 notebooks, and each notebook costs \$6.75. How much will she have to pay at the checkout?',
-    //   ['a) \$31.75', 'b) \$33.50', 'c) \$34.25', 'd) \$35.50', 'e) \$36.75'],
-    //   'c) \$34.25',
-    // ),
-    // Question(
-    //   'Sarah wants to purchase 2 boxes of chocolates. Each box costs \$4.25, but there is a special offer – buy 1, get the second at half price. How much will Sarah pay in total?',
-    //   ['a) \$6.38', 'b) \$7.12', 'c) \$7.88', 'd) \$8.50', 'e) \$9.25'],
-    //   'c) \$7.88',
-    // ),
-    // Question(
-    //   'Identify the smallest value from the following options:',
-    //   ['a) 0.005', 'b) 0.02', 'c) 0.1', 'd) 0.15', 'e) 0.25'],
-    //   'a) 0.005',
-    // ),
-    // Question(
-    //   'Determine the highest value among the following:',
-    //   ['a) 2.5', 'b) 2.05', 'c) 2.15', 'd) 2.25', 'e) 2.35'],
-    //   'a) 2.5',
-    // ),
-    // Question(
-    //   'If 15 is 25% of a certain number, what is that number?',
-    //   ['a) 30', 'b) 40', 'c) 50', 'd) 60', 'e) 70'],
-    //   'b) 60',
-    // ),
-    // Question(
-    //   '72 is 90% of what number?',
-    //   ['a) 75', 'b) 80', 'c) 85', 'd) 90', 'e) 95'],
-    //   'd) 80',
-    // ),
-    // Question(
-    //   'If 80 students attended an event on Monday, and 20% more students attended on Tuesday, how many students attended in total?',
-    //   ['a) 96', 'b) 104', 'c) 112', 'd) 120', 'e) 128'],
-    //   'c) 112',
-    // ),
-    // Question(
-    //   'A store sells hats for \$10 each. If you buy 3 hats, you get a 15% discount. How much will you pay for three hats?',
-    //   ['a) \$25.50', 'b) \$26.50', 'c) \$27.00', 'd) \$28.50', 'e) \$29.00'],
-    //   'd) \$28.50',
-    // ),
-    // Question(
-    //   'If Rachel can run 20 kilometers in 2 hours, and Emma can run the same distance in 1 hour and 40 minutes, how long before Rachel will Emma finish a 15-kilometer race?',
-    //   [
-    //     'a) 10 minutes',
-    //     'b) 15 minutes',
-    //     'c) 20 minutes',
-    //     'd) 25 minutes',
-    //     'e) 30 minutes'
-    //   ],
-    //   'a) 10 minutes',
-    // ),
-    // Question(
-    //   'The average of five numbers is 18. The first four numbers are 20, 22, 16, and 19. What is the missing number?',
-    //   ['a) 13', 'b) 15', 'c) 17', 'd) 21', 'e) 23'],
-    //   'a) 13',
-    // ),
+    Question(
+      'Amy plans to buy 5 notebooks, and each notebook costs \$6.75. How much will she have to pay at the checkout?',
+      ['a) \$31.75', 'b) \$33.50', 'c) \$34.25', 'd) \$35.50', 'e) \$36.75'],
+      'c) \$34.25',
+    ),
+    Question(
+      'Sarah wants to purchase 2 boxes of chocolates. Each box costs \$4.25, but there is a special offer – buy 1, get the second at half price. How much will Sarah pay in total?',
+      ['a) \$6.38', 'b) \$7.12', 'c) \$7.88', 'd) \$8.50', 'e) \$9.25'],
+      'c) \$7.88',
+    ),
+    Question(
+      'Identify the smallest value from the following options:',
+      ['a) 0.005', 'b) 0.02', 'c) 0.1', 'd) 0.15', 'e) 0.25'],
+      'a) 0.005',
+    ),
+    Question(
+      'Determine the highest value among the following:',
+      ['a) 2.5', 'b) 2.05', 'c) 2.15', 'd) 2.25', 'e) 2.35'],
+      'a) 2.5',
+    ),
+    Question(
+      'If 15 is 25% of a certain number, what is that number?',
+      ['a) 30', 'b) 40', 'c) 50', 'd) 60', 'e) 70'],
+      'b) 60',
+    ),
+    Question(
+      '72 is 90% of what number?',
+      ['a) 75', 'b) 80', 'c) 85', 'd) 90', 'e) 95'],
+      'd) 80',
+    ),
+    Question(
+      'If 80 students attended an event on Monday, and 20% more students attended on Tuesday, how many students attended in total?',
+      ['a) 96', 'b) 104', 'c) 112', 'd) 120', 'e) 128'],
+      'c) 112',
+    ),
+    Question(
+      'A store sells hats for \$10 each. If you buy 3 hats, you get a 15% discount. How much will you pay for three hats?',
+      ['a) \$25.50', 'b) \$26.50', 'c) \$27.00', 'd) \$28.50', 'e) \$29.00'],
+      'd) \$28.50',
+    ),
+    Question(
+      'If Rachel can run 20 kilometers in 2 hours, and Emma can run the same distance in 1 hour and 40 minutes, how long before Rachel will Emma finish a 15-kilometer race?',
+      [
+        'a) 10 minutes',
+        'b) 15 minutes',
+        'c) 20 minutes',
+        'd) 25 minutes',
+        'e) 30 minutes'
+      ],
+      'a) 10 minutes',
+    ),
+    Question(
+      'The average of five numbers is 18. The first four numbers are 20, 22, 16, and 19. What is the missing number?',
+      ['a) 13', 'b) 15', 'c) 17', 'd) 21', 'e) 23'],
+      'a) 13',
+    ),
 
-    // /////////////////////////////
-    // Question(
-    //   '1 cookbook is sold for \$9.50. Marie intends to buy 3 cookbooks. How much does she have to pay when she goes to the checkout at the store?',
-    //   ['\$22', '\$23.50', '\$26.50', '\$28', '\$28.50'],
-    //   '\$28.50',
-    // ),
-    // Question(
-    //   'Martin wants to buy a chocolate ice cream. The price of an ice ball is \$1.50, but there is a promotion and for 3 balls purchased, the fourth is for free. How much will Martin have to pay if he buys four scoops of ice cream for himself and four for his girlfriend?',
-    //   ['\$8', '\$8.50', '\$9', '\$10', '\$11.50'],
-    //   '\$10',
-    // ),
-    // Question(
-    //   'Which of the following values is the smallest?',
-    //   ['0.04', '0.09', '1.12', '1.004', '0.05'],
-    //   '0.04',
-    // ),
-    // Question(
-    //   'Which of the following values is the highest?',
-    //   ['1.008', '0.009', '1.08', '1.004', '1.8'],
-    //   '1.8',
-    // ),
-    // Question(
-    //   '20 is 20% of what number?',
-    //   ['50', '100', '200', '250', '300'],
-    //   '100',
-    // ),
-    // Question(
-    //   '48 is 80% of what number?',
-    //   ['50', '55', '60', '65', '70'],
-    //   '60',
-    // ),
-    // Question(
-    //   'On Monday, 50 children bought a ticket for the ghost train attraction. If 30% more children bought a ticket to this attraction on the following Tuesday, how many tickets were sold during those two days?',
-    //   ['30', '65', '85', '115', '165'],
-    //   '65',
-    // ),
-    // Question(
-    //   'One kilo of Persian rice is sold for 5 cents per kilo. For 4 kilos purchased, the seller offers a 20% discount. If Josephine bought 12 kilos of rice, how much did she pay in total?',
-    //   ['\$48', '\$52', '\$58', '\$60', '\$64'],
-    //   '\$58',
-    // ),
-    // Question(
-    //   'It takes Max three hours to run 30 kilometers. Georges, on the other hand, travels the same distance in 2 hours and 20 minutes. If both are to complete a 15-kilometer race, how long before Max will Georges arrive at the finish line?',
-    //   ['10 minutes', '20 minutes', '30 minutes', '40 minutes', '50 minutes'],
-    //   '20 minutes',
-    // ),
-    // Question(
-    //   'A group of four numbers has an average of 14. The first three numbers are 11, 12, and 17. What\'s the missing number?',
-    //   ['11', '13', '15', '16', '18'],
-    //   '14',
-    // ),
-    // Question(
-    //   'Peter has a box containing 65 red and blue balls. Counting them, he realizes that there is a ratio of 3 red balls to 10 blue balls. How many blue balls does he have in total?',
-    //   ['15', '20', '30', '45', '50'],
-    //   '50',
-    // ),
-    // Question(
-    //   'Jean has 5 shirts, 3 ties, 6 trousers, and 2 coats in his wardrobe. How many different sets can he compose using a garment in each category each time?',
-    //   ['60', '80', '180', '200', '220'],
-    //   '180',
-    // ),
-    // Question(
-    //   'What would be the next number in the following series? 1 … 10 … 2 … 9 … 3 … ?',
-    //   ['5', '8', '10', '11', '12'],
-    //   '8',
-    // ),
-    // Question(
-    //   'What would be the next number in the following series? 1 … 3 … 7 … 15 … 31 … ?',
-    //   ['63', '75', '83', '92', '94'],
-    //   '63',
-    // ),
+    /////////////////////////////
+    Question(
+      '1 cookbook is sold for \$9.50. Marie intends to buy 3 cookbooks. How much does she have to pay when she goes to the checkout at the store?',
+      ['\$22', '\$23.50', '\$26.50', '\$28', '\$28.50'],
+      '\$28.50',
+    ),
+    Question(
+      'Martin wants to buy a chocolate ice cream. The price of an ice ball is \$1.50, but there is a promotion and for 3 balls purchased, the fourth is for free. How much will Martin have to pay if he buys four scoops of ice cream for himself and four for his girlfriend?',
+      ['\$8', '\$8.50', '\$9', '\$10', '\$11.50'],
+      '\$10',
+    ),
+    Question(
+      'Which of the following values is the smallest?',
+      ['0.04', '0.09', '1.12', '1.004', '0.05'],
+      '0.04',
+    ),
+    Question(
+      'Which of the following values is the highest?',
+      ['1.008', '0.009', '1.08', '1.004', '1.8'],
+      '1.8',
+    ),
+    Question(
+      '20 is 20% of what number?',
+      ['50', '100', '200', '250', '300'],
+      '100',
+    ),
+    Question(
+      '48 is 80% of what number?',
+      ['50', '55', '60', '65', '70'],
+      '60',
+    ),
+    Question(
+      'On Monday, 50 children bought a ticket for the ghost train attraction. If 30% more children bought a ticket to this attraction on the following Tuesday, how many tickets were sold during those two days?',
+      ['30', '65', '85', '115', '165'],
+      '65',
+    ),
+    Question(
+      'One kilo of Persian rice is sold for 5 cents per kilo. For 4 kilos purchased, the seller offers a 20% discount. If Josephine bought 12 kilos of rice, how much did she pay in total?',
+      ['\$48', '\$52', '\$58', '\$60', '\$64'],
+      '\$58',
+    ),
+    Question(
+      'It takes Max three hours to run 30 kilometers. Georges, on the other hand, travels the same distance in 2 hours and 20 minutes. If both are to complete a 15-kilometer race, how long before Max will Georges arrive at the finish line?',
+      ['10 minutes', '20 minutes', '30 minutes', '40 minutes', '50 minutes'],
+      '20 minutes',
+    ),
+    Question(
+      'A group of four numbers has an average of 14. The first three numbers are 11, 12, and 17. What\'s the missing number?',
+      ['11', '13', '15', '16', '18'],
+      '14',
+    ),
+    Question(
+      'Peter has a box containing 65 red and blue balls. Counting them, he realizes that there is a ratio of 3 red balls to 10 blue balls. How many blue balls does he have in total?',
+      ['15', '20', '30', '45', '50'],
+      '50',
+    ),
+    Question(
+      'Jean has 5 shirts, 3 ties, 6 trousers, and 2 coats in his wardrobe. How many different sets can he compose using a garment in each category each time?',
+      ['60', '80', '180', '200', '220'],
+      '180',
+    ),
+    Question(
+      'What would be the next number in the following series? 1 … 10 … 2 … 9 … 3 … ?',
+      ['5', '8', '10', '11', '12'],
+      '8',
+    ),
+    Question(
+      'What would be the next number in the following series? 1 … 3 … 7 … 15 … 31 … ?',
+      ['63', '75', '83', '92', '94'],
+      '63',
+    ),
   ];
 }
